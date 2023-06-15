0310bcec1b793698d0b90286f2b68d33df8c8d58
Evandher <evandher24@gmail.com>
2023-06-14 19:01:33 -0500

    Correction

--- seeds/index.js
+++ seeds/index.js
@@ -15,9 +15,10 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for(let i = 0 ; i< 50; i++){
         const random1000 = Math.floor(Math.random() * 1000)
-        new Campground({
+        const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`
         })
+        await camp.save()
     }
 }
 
