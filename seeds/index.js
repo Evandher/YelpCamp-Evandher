cb6a213a104458b3e2bbb2437e35ba2046ca723a
Evandher <evandher24@gmail.com>
2023-06-29 14:01:01 -0500

    Adding layouts and template styles

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () =>{
     await Campground.deleteMany({});
     for(let i = 0 ; i< 50; i++){
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum iusto quae repudiandae harum itaque iste a ducimus maiores non fuga nam, delectus odio! Ad minus magni perspiciatis? Accusamus, libero eum.',
+            price
         })
         await camp.save()
     }
